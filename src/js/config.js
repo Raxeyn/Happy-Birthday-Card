1db4f8de51723b4f6c859eb69454430157876965
Raxeyn <56265569+Raxeyn@users.noreply.github.com>
2024-01-09 17:48:32 +0300

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,11 +1,11 @@
 export default {
-  name: process.env.NAME, // actual name of the recipient (Mandatory)
+  name: "Yüsra", // actual name of the recipient (Mandatory)
 
-  nickname: process.env.NICKNAME, // nickname(optional)
+  nickname: "Yüsra", // nickname(optional)
 
-  pic: process.env.PIC, // image url of recipients (Mandatory)
+  pic: "https://cdn.discordapp.com/attachments/1164141329819910155/1194184876673466458/3722-minecraft-heart.png?ex=65af6e55&is=659cf955&hm=8ced765c4eac287dce4e3a334a38f52666adccccbd67dec7f533bf9a9ec95887&", // image url of recipients (Mandatory)
 
   showScrollMsg: process.env.SCROLL_MSG, // set to false if you do not want the scrolling message
 
-  birthDate: process.env.BIRTH_DATE, // Mention birthday date in YYYY-MM-DD format
+  birthDate: "19 Aralık 2007", // Mention birthday date in YYYY-MM-DD format
 };
